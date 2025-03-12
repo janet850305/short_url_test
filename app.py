08691d0a4df3cc14e92f5f5d5db631a7c4a6b640
janet850305 <janet850305@gmail.com>
2025-03-12 22:33:56 +0800

    change create database place

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ import time
 from datetime import datetime
 DB_FILE = "short_urls.db"
 
+#create sqlite database table
+def init_db(): 
+    if not os.path.exists(DB_FILE):
+        conn = sqlite3.connect(DB_FILE)
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS urls (
+                short_url TEXT PRIMARY KEY,
+                original_url TEXT NOT NULL,
+                expiration_date INTEGER NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+        print("SQLite already build!")
+    else:
+        print("SQLite already exist!")
+init_db()  
 
 
 def save_url(short_url, original_url, expiration_date):
